0e733fbca96ab786de788034bc87da9261dc6b2a
joerg.egger <joerg.egger@outlook.de>
2014-07-27 09:44:45 +0200

    DetailViewGenerator Labels

--- Trigger.XForms/Visuals/DetailViewGenerator.cs
+++ Trigger.XForms/Visuals/DetailViewGenerator.cs
@@ -43,12 +43,13 @@ namespace Trigger.XForms.Visuals
                     if (visibilityAttribute != null && (visibilityAttribute.TargetView == TargetView.ListOnly || visibilityAttribute.TargetView == TargetView.None))
                         continue;
 
+                    layout.BeginHorizontal();
+
                     if (property.PropertyType.IsGenericType)
                     {
                         var value = property.GetValue(Model, null);
                         if (value is IEnumerable<IStorable>)
                         {
-
                             var firstItem = (value as IEnumerable<IStorable>).FirstOrDefault();
 
                             if (firstItem != null)
@@ -64,39 +65,32 @@ namespace Trigger.XForms.Visuals
 
                                 gridView.Size = new Eto.Drawing.Size(-1, height);
                                 layout.Add(gridView, true, false);
-                                layout.EndHorizontal();
-
                             }
                         }
-
                     }
 
                     if (property.PropertyType == typeof(string))
                     {
                         AddLabel(layout, property);
-                        layout.Add(EditorFactory.StringPropertyEditor(property), true);
-                        layout.EndHorizontal();
+                        layout.Add(EditorFactory.StringPropertyEditor(property), true, false);
                     }
 
                     if (property.PropertyType == typeof(DateTime?) || property.PropertyType == typeof(DateTime))
                     {
                         AddLabel(layout, property);
-                        layout.Add(EditorFactory.DateTimePropertyEditor(property), true);
-                        layout.EndHorizontal();
+                        layout.Add(EditorFactory.DateTimePropertyEditor(property), true, false);
                     }
 
                     if (property.PropertyType == typeof(TimeSpan?) || property.PropertyType == typeof(TimeSpan))
                     {
                         AddLabel(layout, property);
-                        layout.Add(EditorFactory.TimeSpanPropertyEditor(property), true);
-                        layout.EndHorizontal();
+                        layout.Add(EditorFactory.TimeSpanPropertyEditor(property), true, false);
                     }
 
                     if (property.PropertyType == typeof(bool))
                     {
                         AddLabel(layout, property);
-                        layout.Add(EditorFactory.BooleanPropertyEditor(property), true);
-                        layout.EndHorizontal();
+                        layout.Add(EditorFactory.BooleanPropertyEditor(property), true, false);
                     }
 
                     if (typeof(IStorable).IsAssignableFrom(property.PropertyType))
@@ -104,32 +98,27 @@ namespace Trigger.XForms.Visuals
                         AddLabel(layout, property);
                         var referenceComboBox = EditorFactory.ReferencePropertyEditor(property);
 
-                        layout.Add(referenceComboBox, true);
+                        layout.Add(referenceComboBox, true, false);
 
                         //AddReferenceButtons(layout, referenceComboBox);
-
-                        layout.EndHorizontal();
                     }
 
                     if (property.PropertyType == typeof(int) || property.PropertyType == typeof(double) || property.PropertyType == typeof(decimal))
                     {
                         AddLabel(layout, property);
-                        layout.Add(EditorFactory.NumberPropertyEditor(property), true);
-                        layout.EndHorizontal();
+                        layout.Add(EditorFactory.NumberPropertyEditor(property), true, false);
                     }
 
                     if (property.PropertyType.BaseType == typeof(Enum))
                     {
                         AddLabel(layout, property);
-                        layout.Add(EditorFactory.EnumPropertyEditor(property), true);
-                        layout.EndHorizontal();
+                        layout.Add(EditorFactory.EnumPropertyEditor(property), true, false);                       
                     }
+
+                    layout.EndHorizontal();
                 }
             }
-
-            layout.BeginHorizontal();
-            layout.EndHorizontal();
-		
+               
             return layout;
         }
 
@@ -137,17 +126,14 @@ namespace Trigger.XForms.Visuals
         {
             var attribute = property.GetCustomAttributes(typeof(System.ComponentModel.DisplayNameAttribute), true).FirstOrDefault() as System.ComponentModel.DisplayNameAttribute;
 
-            layout.BeginVertical();
             var label = new Label
             {
-                Text = (attribute != null ? attribute.DisplayName : property.Name) + ":"
+                Text = (attribute != null ? attribute.DisplayName : property.Name) + ":",
+                Size = new Size(120, -1),
+                Wrap = WrapMode.Word
             };
 
-            //label.Size = new Size(100, -1);
-            //label.Font = new Eto.Drawing.Font(label.Font.Family, 8f);
             layout.Add(label, false, true);
-            layout.EndVertical();
-            layout.BeginHorizontal();
         }
 
         void AddReferenceButtons(DynamicLayout layout, ComboBox referenceComboBox)
